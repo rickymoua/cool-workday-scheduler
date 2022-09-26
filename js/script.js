aa2f47f9ce26faceb1acc1407760726fc344c0e2
ricky moua <rmoua018@gmail.com>
2022-09-25 20:36:44 -0500

    id of hours added to html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+// Date and time using moment.js
 function date_time() {
-    now = moment().format('dddd Do MMMM YYYY h:mm:ss a');
+    now = moment().format('dddd Do MMMM YYYY hh:mm:ss a');
     document.getElementById('time-display').innerHTML = now;
     setTimeout(function () { date_time(); }, 
     
